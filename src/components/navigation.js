82e065e6f1977b1c8c91a358ebd5e263b876eaa7
MNR_Dev <mnrdev510@gmail.com>
2022-06-30 12:04:40 -0700

    adding components

---
+++ src/components/navigation.js
@@ -0,0 +1,13 @@
+import {Link}from "react-router-dom";
+
+const Navigation = (props) => {
+    return (
+        <div>
+            Navigation
+            <Link to='/'>Home</Link>
+            
+        </div>
+    )
+}
+
+export default Navigation;
