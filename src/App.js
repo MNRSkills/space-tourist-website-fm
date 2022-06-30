68ada15365ae0ffa1524809a7644591d5f509cd6
MNR_Dev <mnrdev510@gmail.com>
2022-06-30 12:04:10 -0700

    Second commit with Router action

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-
-
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/pages/home";
+import Navigation from "./components/navigation";
 const App = () => {
   return (
-    <div className="App">
-     <h1> hello world</h1>
+    <div className='App'>
+      <Navigation/>
+      <Routes>
+        <Route path='/' elemant={Home}/>
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
